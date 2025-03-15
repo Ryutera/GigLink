cdf66b2f3344d29a74737a07bb0374ce587fc125
Ryutera <kuya9823@gmail.com>
2025-03-15 01:24:08 +0000

    ongoingevetmap rechecked

--- app/components/join/OngoingEventMap.tsx
+++ app/components/join/OngoingEventMap.tsx
@@ -1,89 +1,131 @@
-"use client"
+"use client";
 
-import {AdvancedMarker, APIProvider, InfoWindow, Map, MapCameraChangedEvent, Pin} from '@vis.gl/react-google-maps';
-import Link from 'next/link';
+import { MusicEvent } from "@/types/events";
+import {
+  AdvancedMarker,
+  APIProvider,
+  InfoWindow,
+  Map,
+  MapCameraChangedEvent,
+  Pin,
+} from "@vis.gl/react-google-maps";
+import Link from "next/link";
 
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-
-interface Props{
-events:any;
-userId:string|null
+interface Props {
+  events: MusicEvent[];
+  userId: string | null;
 }
-const OngoingEventMap = ({events,userId}:Props) => {
-  const [selectedEvent, setSelectedEvent] = useState<any>(null);
-  console.log(selectedEvent,"pin clicked")
+interface LocationProps{
+  key: string
+  location: { lat: number, lng: number}
+  isOrganizer: boolean
+  hasApplied: boolean
+  title: string
+  date: Date
+  location_name: string
+  event: MusicEvent
+}
+
+const OngoingEventMap = ({ events, userId }: Props) => {
+  const [selectedEvent, setSelectedEvent] = useState<LocationProps | null>(null);
+  console.log(selectedEvent, "pin clicked");
 
   //ロケーションのサンプル
 
-
-  
-  const locations = events.map((event: any) => ({
+  const locations = events.map((event) => ({
     key: event.id,
     location: { lat: event.latitude, lng: event.longitude },
     isOrganizer: event.organizerId === userId,
-    hasApplied: event.applications.some((app: any) => app.userId === userId),
+    hasApplied: event.applications.some((app) => app.userId === userId),
     title: event.title,
     date: event.date,
     location_name: event.location,
     event: event,
-  }))
-  const handleMarkerClick = (location: any) => {
-    setSelectedEvent(location)
-  }
+  }));
 
-
+  const handleMarkerClick = (location:LocationProps) => {
+    setSelectedEvent(location);
+  };
 
   const handleClose = () => setSelectedEvent(null);
 
-
   return (
-    
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string} onLoad={() => console.log('Maps API has loaded.')}>
-      <div className='flex w-screen h-screen '>
-   <Map
-   
-      defaultZoom={10}
-      defaultCenter={  { lat: 51.5074, lng: -0.1278 } }
-       mapId='EVENTS_MAP'
-      onCameraChanged={ (ev: MapCameraChangedEvent) =>
-        console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-      }>
+    <APIProvider
+      apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}
+      onLoad={() => console.log("Maps API has loaded.")}
+    >
+      <div className="flex w-screen h-screen ">
+        <Map
+          defaultZoom={10}
+          defaultCenter={{ lat: 51.5074, lng: -0.1278 }}
+          mapId="EVENTS_MAP"
+          onCameraChanged={(ev: MapCameraChangedEvent) =>
+            console.log(
+              "camera changed:",
+              ev.detail.center,
+              "zoom:",
+              ev.detail.zoom
+            )
+          }
+        >
+          {locations.map((location: LocationProps) => (
+            <AdvancedMarker
+              key={location.key}
+              position={location.location}
+              onClick={() => handleMarkerClick(location)}
+            >
+              {/* //ここの条件文は緯度経度無いやつが赤道に表示されるのを防ぐため */}
+              {location.location.lat === 0 && location.location.lng === 0 ? (
+                <></>
+              ) : location.isOrganizer ? (
+                <Pin
+                  background={"green"}
+                  glyphColor={"#000"}
+                  borderColor={"#000"}
+                />
+              ) : location.hasApplied ? (
+                <Pin
+                  background={"gray"}
+                  glyphColor={"#000"}
+                  borderColor={"#000"}
+                />
+              ) : (
+                <Pin
+                  background={"blue"}
+                  glyphColor={"#000"}
+                  borderColor={"#000"}
+                />
+              )}
+            </AdvancedMarker>
+          ))}
 
-        {locations.map((location:any)=>(
-<AdvancedMarker
-key={location.key}
-position={location.location}
-onClick={()=>handleMarkerClick(location
-)}>
-  {/* //ここの条件文は緯度経度無いやつが赤道に表示されるのを防ぐため */}
-  {(location.location.lat === 0 && location.location.lng===0)? <></>:
-  location.isOrganizer?
-  <Pin  background={'green'} glyphColor={'#000'} borderColor={'#000'} /> : 
-    location.hasApplied? 
-    <Pin  background={'gray'} glyphColor={'#000'} borderColor={'#000'} /> : 
-    
-    <Pin  background={'blue'} glyphColor={'#000'} borderColor={'#000'} /> 
-
-  }
-
-</AdvancedMarker>
-        )) }
-
-{selectedEvent!==null && (
-        <InfoWindow onCloseClick={handleClose} position={selectedEvent.location} >
-          <div className="p-2 max-w-xs ">
-                <h3 className="font-bold text-lg mb-1">{selectedEvent.title}</h3>
+          {selectedEvent !== null && (
+            <InfoWindow
+              onCloseClick={handleClose}
+              position={selectedEvent.location}
+            >
+              <div className="p-2 max-w-xs ">
+                <h3 className="font-bold text-lg mb-1">
+                  {selectedEvent.title}
+                </h3>
                 <p className="text-sm mb-1">
-                  <span className="font-medium">日時:</span> {new Date(selectedEvent.event.date).toLocaleDateString()}
+                  <span className="font-medium">日時:</span>{" "}
+                  {new Date(selectedEvent.event.date).toLocaleDateString()}
                 </p>
                 <p className="text-sm mb-1">
-                  <span className="font-medium">場所:</span> {selectedEvent.location_name}
+                  <span className="font-medium">場所:</span>{" "}
+                  {selectedEvent.location_name}
                 </p>
                 <p className="text-sm mb-2">
                   <span className="font-medium">ステータス:</span>{" "}
-                  {selectedEvent.isOrganizer ? "主催者" : selectedEvent.hasApplied ? "応募済み" : "未応募"}
+                  {selectedEvent.isOrganizer
+                    ? "主催者"
+                    : selectedEvent.hasApplied
+                    ? "応募済み"
+                    : "未応募"}
                 </p>
                 <Link
                   href={`/event_detail/${selectedEvent.key}`}
@@ -92,15 +134,12 @@ onClick={()=>handleMarkerClick(location
                   <p>詳細を見る</p>
                 </Link>
               </div>
-        </InfoWindow>
-      )}
-        
-        
-   </Map>
-   </div>
- </APIProvider>
+            </InfoWindow>
+          )}
+        </Map>
+      </div>
+    </APIProvider>
+  );
+};
 
-  )
-}
-
-export default OngoingEventMap
+export default OngoingEventMap;
--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,83 +1,78 @@
+import ProfileForm from "@/app/components/profile/ProfileForm";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import ApplicationStatus from "@/app/components/ApplicationStatus";
 
-import ProfileForm from '@/app/components/profile/ProfileForm'
-import prisma from '@/lib/prisma'
-import { auth } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import ApplicationStatus from '@/app/components/ApplicationStatus'
+const ProfilePage = async ({ params }: { params: { id: string } }) => {
+  const { userId } = await auth();
 
+  const { id } = await params;
+  const paramsUserID = id;
 
-
-
-const ProfilePage = async ({params}:{params:{id:string}}) => {
-  const {userId} = await auth()
-
-  const {id} = await params
-  const paramsUserID = id
-
-    
   if (!userId) {
-    redirect('/sign-up')
+    redirect("/sign-up");
   }
   const userInfo = await prisma.user.findUnique({
     where: {
-      id: paramsUserID
+      id: paramsUserID,
     },
-  })
+  });
 
   if (!userInfo) {
-    return
+    return;
   }
-  console.log( paramsUserID, userId)
+  console.log(paramsUserID, userId);
 
-  const schedules = await prisma.application.findMany(
-   {
-    where:{
-      userId:userId,
+  const schedules = await prisma.application.findMany({
+    where: {
+      userId: userId,
       event: {
         startTime: {
-          gt: new Date()
+          gt: new Date(),
         },
       },
-     
-    },include:{
-      event:true ,     
     },
-   }
-  )
+    include: {
+      event: true,
+    },
+  });
 
   const organizedEvents = await prisma.event.findMany({
-    where:{organizerId:userInfo.id}
-  })
+    where: { organizerId: userInfo.id },
+  });
 
   const applications = await prisma.application.findMany({
-    where:{userId:userInfo.id}
-  })
-
-
+    where: { userId: userInfo.id },
+  });
 
   return (
-  <div className="flex  justify-center w-full p-7">
-  <Tabs defaultValue="account" className="w-full max-w-2xl">
-    {
-  
-    userId===paramsUserID&&
-    
-    <TabsList className="mb-4">
-    
-    <TabsTrigger value="account">Account</TabsTrigger>
-    <TabsTrigger value="appStatus">Application Status</TabsTrigger>
-    
-  </TabsList>}
-  
-  <TabsContent value="account" ><ProfileForm user={userInfo} userId={userId} organizedEvents={organizedEvents}  applications={ applications }/></TabsContent>
-  <TabsContent value="appStatus" ><ApplicationStatus schedules={schedules}/></TabsContent>
-</Tabs>
-</div>
-  
-  
-    
-  )
-}
+    <div className="flex items-center justify-center w-full p-7 ">
+      <Tabs defaultValue="account" className="w-full max-w-2xl">
+        {userId === paramsUserID && (
+          <div className="flex justify-center w-full mb-8">
+          <TabsList>
+            <TabsTrigger value="account">Account</TabsTrigger>
+            <TabsTrigger value="appStatus">Application Status</TabsTrigger>
+          </TabsList>
+        </div>
+        )}
 
-export default ProfilePage
+        <TabsContent value="account">
+          <ProfileForm
+            user={userInfo}
+            userId={userId}
+            organizedEvents={organizedEvents}
+            applications={applications}
+          />
+        </TabsContent>
+        <TabsContent value="appStatus">
+          <ApplicationStatus schedules={schedules} />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default ProfilePage;
