137363d5003440b2bb15944062e7136b6dff7164
Ryutera <kuya9823@gmail.com>
2025-03-19 22:42:29 +0000

    .

--- app/components/CustomInput.tsx
+++ app/components/CustomInput.tsx
@@ -9,7 +9,7 @@ interface CustomInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 const CustomInput: React.FC<CustomInputProps> = ({ label, type, isEditable, className, ...props }) => {
   return (
     <div>
-      <label htmlFor={props.id} className="block text-sm font-medium text-red-500 mb-1">
+      <label htmlFor={props.id} className="block text-sm font-medium text-gray-900 mb-1">
         {label}
       </label>
       <input
@@ -17,7 +17,7 @@ const CustomInput: React.FC<CustomInputProps> = ({ label, type, isEditable, clas
         disabled={!isEditable}
         type={type}
         className={cn(
-          "w-full px-3 py-2 text-gray-900",
+          "w-full px-3 py-2 text-gray-900 disabled:text-gray-900 disabled:opacity-100)",
           isEditable
             ? "border shadow-sm rounded-md  border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
             : "bg-white cursor-default ",
