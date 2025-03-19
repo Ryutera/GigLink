58f7d475ba12f76127b26d6ef54d2258ef5831b5
Ryutera <kuya9823@gmail.com>
2025-03-19 22:31:29 +0000

    opacity-100 addedt to disabled - CustomInput

--- app/components/CustomInput.tsx
+++ app/components/CustomInput.tsx
@@ -17,7 +17,7 @@ const CustomInput: React.FC<CustomInputProps> = ({ label, type, isEditable, clas
         disabled={!isEditable}
         type={type}
         className={cn(
-          "w-full px-3 py-2 text-red-500 disabled:text-gray-900",
+          "w-full px-3 py-2 text-red-500 disabled:(text-gray-900 opacity-100)",
           isEditable
             ? "border shadow-sm rounded-md  border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
             : "bg-white cursor-default ",
