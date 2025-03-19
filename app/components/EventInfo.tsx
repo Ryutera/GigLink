ea1ab9635557e768e9166eae3f6bf7450c922847
Ryutera <kuya9823@gmail.com>
2025-03-19 23:02:54 +0000

    .

--- app/components/EventInfo.tsx
+++ app/components/EventInfo.tsx
@@ -115,7 +115,7 @@ const EventInfo = ({ event, userId, onSubmit }: any) => {
                 onChange={(e) => setLocation(e.target.value)}
                 disabled={!isOrganizer}
                 className={`
-                  w-full px-3 py-2 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
+                  w-full px-3 py-2 !text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
                   ${isOrganizer ? "border border-gray-300" : "bg-white cursor-default"}
                 `}
               />
