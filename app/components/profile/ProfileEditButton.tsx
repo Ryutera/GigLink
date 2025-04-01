3e929f2dc63d6049185d3903ff92ef99a85243e7
Ryutera <kuya9823@gmail.com>
2025-04-01 23:36:33 +0100

    .

--- app/components/profile/ProfileEditButton.tsx
+++ app/components/profile/ProfileEditButton.tsx
@@ -10,7 +10,7 @@ const ProfileEditButton = () => {
       className="px-6 py-3 bg-gradient-to-r disabled:bg-gray-500  bg-blue-600 text-white font-medium rounded-lg shadow-md hover:from-blue-600 hover:to-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transform transition hover:-translate-y-0.5 active:translate-y-0 "
      
     >
-      {pending?"更新中です...":"プロフィールを更新する"}
+      {pending ? "Updating..." : "Update Profile"}
     </button>
     )
 }
